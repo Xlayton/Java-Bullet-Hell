7b6fa75cc199ce5f48ef0acb4acadee2d1234cbf
muhahaha100 <oscarbaso@gmail.com>
2019-02-26 10:18:14 -0700

    Moved code from controller to view
    Whoops, did gameView stuff on GameController

--- java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/GameController.java
+++ java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/GameController.java
@@ -1,54 +1,5 @@
 package edu.neumont.hellraisers.javabullethell;
 
-import edu.neumont.hellraisers.javabullethell.model.Board;
-import edu.neumont.hellraisers.javabullethell.model.Enemy;
-import edu.neumont.hellraisers.javabullethell.model.Entity;
-import edu.neumont.hellraisers.javabullethell.model.Player;
-import edu.neumont.hellraisers.javabullethell.model.Projectile;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-
-public class GameController {
-	private Scene view;
-	private Group group;
-	private Canvas canvas;
-	private GraphicsContext context;
-	private GameController control;
-	
-	public GameController() {
-		
-	}
-	
-	public void updateDisplay(Board board) {
-		
-	}
-	
-	public void updateController(GameController control) {
-		this.control = control;
-	}
-	
-	private void drawPlayer(Player player) {
-		
-	}
-	
-	private void drawEnemy(Enemy enemy) {
-		
-	}
-	
-	private void drawProjectiles(Projectile[] projectiles) {
-		
-	}
-	
-	private void drawScore(Player player) {
-		
-	}
-	
-	private void drawHealth(Entity entity) {
-		
-	}
-	
-	
+class GameController {
 	
 }
--- java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/ui/GameView.java
+++ java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/ui/GameView.java
@@ -1,5 +1,48 @@
-package edu.neumont.hellraisers.javabullethell.ui;
+
+import edu.neumont.hellraisers.javabullethell.GameController;
+import edu.neumont.hellraisers.javabullethell.model.Board;
+import edu.neumont.hellraisers.javabullethell.model.Enemy;
+import edu.neumont.hellraisers.javabullethell.model.Entity;
+import edu.neumont.hellraisers.javabullethell.model.Player;
+import edu.neumont.hellraisers.javabullethell.model.Projectile;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 public class GameView extends View{
-
+	private Scene view;
+	private Group group;
+	private Canvas canvas;
+	private GraphicsContext context;
+	private GameController control;
+	
+	public void updateDisplay(Board board) {
+		
+	}
+	
+	public void updateController(GameController control) {
+		this.control = control;
+	}
+	
+	private void drawPlayer(Player player) {
+		
+	}
+	
+	private void drawEnemy(Enemy enemy) {
+		
+	}
+	
+	private void drawProjectiles(Projectile[] projectiles) {
+		
+	}
+	
+	private void drawScore(Player player) {
+		
+	}
+	
+	private void drawHealth(Entity entity) {
+		
+	}
+	
 }
