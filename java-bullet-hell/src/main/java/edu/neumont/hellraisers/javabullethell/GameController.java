342ae8c7b867dbd0bd3d45bbb258a740f20e8654
Xlayton <weightequalsmasstimesgravity@gmail.com>
2019-02-27 10:48:12 -0700

    Commit today's work
    Worked on OptionView, a couple changes to MainView

--- java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/GameController.java
+++ java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/GameController.java
@@ -1,15 +1,29 @@
 package edu.neumont.hellraisers.javabullethell;
 
+import java.io.IOException;
+
 import edu.neumont.hellraisers.javabullethell.model.Board;
+import edu.neumont.hellraisers.javabullethell.model.SceneSelection;
 import edu.neumont.hellraisers.javabullethell.ui.MainView;
 
 public class GameController {
 	private Board board;
 	private MainView mainView;
+	private double difficulty = 2.0;
+	private double sound = 75.0;
 	
 	public GameController(MainView view) {
 		this.mainView = view;
 		this.board = new Board();
+		mainView.registerController(this);
+		try {
+			mainView.init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//YUP
+		mainView.switchScene(SceneSelection.OPTION_VIEW);
+		// OH YEAH YEAH
 	}
 	
 	public void init() {
@@ -32,6 +46,12 @@ public class GameController {
 		
 	}
 	
+	public void onApply(double difficulty, double sound) {
+		this.difficulty = difficulty;
+		this.sound = sound;
+		System.out.println(this.difficulty + " " + this.sound);
+	}
+	
 	public void createEnemy() {
 		
 	}
@@ -47,5 +67,14 @@ public class GameController {
 	public void removePlayer() {
 		
 	}
+	
+	public double getDifficulty() {
+		return difficulty;
+	}
+	
+	public double getSound(){
+		return sound;
+	}
+	
 	
 }
--- java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/ui/MainView.java
+++ java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/ui/MainView.java
@@ -1,7 +1,12 @@
 package edu.neumont.hellraisers.javabullethell.ui;
 
+import java.io.IOException;
+
 import edu.neumont.hellraisers.javabullethell.GameController;
 import edu.neumont.hellraisers.javabullethell.model.SceneSelection;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainView {
@@ -12,6 +17,14 @@ public class MainView {
 	private GameView gameView;
 	private EndView endView;
 	private OptionView optionView;
+	
+	public void init() throws IOException {
+		FXMLLoader optionLoad = new FXMLLoader(this.getClass().getClassLoader().getResource("OptionView.fxml"));
+		Parent optionParent = optionLoad.load();
+		this.optionView = optionLoad.getController();
+		optionView.setView(new Scene(optionParent));
+		optionView.registerController(control);
+	}
 	
 	public void switchScene(SceneSelection selection){
 		switch(selection) {
