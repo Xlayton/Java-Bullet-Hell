691320f6b86e2ba83e30e97519da1f3f79f33c8c
Xlayton <weightequalsmasstimesgravity@gmail.com>
2019-03-04 10:16:58 -0700

    Add movement functionality to enemies
    Also make enemy shoot at player

--- java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/model/Enemy.java
+++ java-bullet-hell/src/main/java/edu/neumont/hellraisers/javabullethell/model/Enemy.java
@@ -1,5 +1,7 @@
 package edu.neumont.hellraisers.javabullethell.model;
 
+import java.util.Random;
+
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -9,17 +11,53 @@ public class Enemy extends Entity{
 	
 	private EnemyType enemyType;
 	private Timeline shootTimer;
+	private Timeline moveTimer;
 	private int numShotsToShoot;
+	boolean moveUp = false;
+	boolean moveLeft = false;
 	
 	public Enemy(EnemyType enemyType, Coordinate location, double difficulty) {
 		super(enemyType.getHealth(), enemyType.getWidth(), enemyType.getHeight(), location);
 		this.enemyType = enemyType;
 		this.numShotsToShoot = 0;
-		shootTimer = new Timeline(new KeyFrame(Duration.millis(600/difficulty), e -> {
+		this.shootTimer = new Timeline(new KeyFrame(Duration.millis(1200/difficulty), e -> {
 			numShotsToShoot++;
 		}));
 		shootTimer.setCycleCount(Animation.INDEFINITE);
 		shootTimer.play();
+		this.moveTimer = new Timeline(new KeyFrame(Duration.millis(25), e -> {
+			if(this.getLocation().getX() >= 800) {
+				moveLeft = true;
+			} else if (this.getLocation().getX() <= 0) {
+				moveLeft = false;
+			}
+			if(this.getLocation().getY() >= 800) {
+				moveUp = true;
+			} else if (this.getLocation().getY() <= 0) {
+				moveUp = false;
+			}
+			
+			if(new Random().nextInt(100) + 1 == 1) {
+				moveUp = !moveUp;
+				moveLeft = ! moveLeft;
+			}
+			
+			if(moveUp && moveLeft) {
+				this.getLocation().addX(-2);
+				this.getLocation().addY(-2);
+			} else if (moveUp && !moveLeft) {
+				this.getLocation().addX(2);
+				this.getLocation().addY(-2);
+			} else if (!moveUp && moveLeft) {
+				this.getLocation().addX(-2);
+				this.getLocation().addY(2);
+			} else {
+				this.getLocation().addX(2);
+				this.getLocation().addY(2);
+			}
+		}));
+		moveTimer.setCycleCount(Animation.INDEFINITE);
+		moveTimer.play();
 	}
 	
 	@Override
